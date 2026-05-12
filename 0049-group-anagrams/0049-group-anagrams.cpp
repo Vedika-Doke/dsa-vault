bce6de95626a69d94620304e60258e5012f1729a
Vedika <vedikapdoke@gmail.com>
2026-05-12 11:10:18 +0530

    Time: 18 ms (49.34%), Space: 26.2 MB (37.63%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> cabinet;
+        for(string s:strs){
+            string temp = s;
+            sort(temp.begin(), temp.end());
+            cabinet[temp].push_back(s);
+        }
+        vector<vector<string>> res;
+        for(auto [label,stringy] : cabinet){
+            res.push_back(stringy);
+        }
+        return res;
+    }
+};
